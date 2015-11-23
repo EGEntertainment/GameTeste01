b5bfd8869d0f80a4efdadb8ea3e1a45405b13ec1
EGEntertainment <eletronicgeneration@gmail.com>
2015-11-23 20:45:49 -0200

    NavMesh + LookMouse

---
+++ ProjetoNavMesh/Assets/Scripts/CharacterMovmentController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovmentController : MonoBehaviour {
+
+    //Inspector Variable
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float speed;
+
+
+    //Private Variables
+    private float verticalInput;
+    private float horizontalInput;
+    private Vector3 lookingPosition;
+    private NavMeshAgent navMeshAgent;
+
+
+    protected virtual void Awake()
+    {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+	protected virtual void Update ()
+    {
+        DetectInput();
+        MoveCharacter();
+        LookPointer();	
+	}
+
+    private void LookPointer()
+    {
+        Quaternion LookRotation = Quaternion.LookRotation(transform.position - lookingPosition, transform.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, LookRotation, speed * Time.deltaTime);
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 targetPosition = new Vector3(horizontalInput * speed, 0, verticalInput * speed);
+        navMeshAgent.Move(targetPosition * Time.deltaTime);
+    }
+
+    private void DetectInput()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Ray cameraToGround = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if(Physics.Raycast(cameraToGround, out hit, 50.0f, groundMask))
+
+            lookingPosition = hit.point;
+    }
+}
---
+++ ProjetoNavMesh/Assets/Scripts/Identify.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Identify : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider Hero){
+
+	   Debug.Log ("Abrir Caixa de Dialogo");					 
+
+	}
+
+	void OnTriggerExit (Collider Hero){
+
+	   Debug.Log ("Fechar Caixa de Dialogo");					 
+			
+	}
+}
